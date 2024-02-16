6da2f568bb859a289b7a793f52fffb8ea9c8b7a6
catherine-tranchand <catherine.tranchand@laplateforme.io>
2024-02-16 13:23:43 +0100

    init app route

---
+++ app/route.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+export async function GET(request) {
+    return new Response("welcome to clickunap API", {
+        status: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+          },
+    })
+}
