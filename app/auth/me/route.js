eebd5ceeb2a78556a16d6425825759d7bf4e4340
catherine-tranchand <catherine.tranchand@laplateforme.io>
2025-01-31 16:27:26 +0100

    :fire: update me route

--- app/auth/me/route.js
+++ app/auth/me/route.js
@@ -1,6 +1,8 @@
-
+import { sql } from "@vercel/postgres";
+import { recoverEmailPassword } from "@/utils/jwt"; 
 
 // route to find the user with an id
+/*
 export async function POST(request){
     const formData = await request.formData();
 
@@ -15,4 +17,47 @@ export async function POST(request){
 
     return Response.json({data: me});
 
+}
+*/
+
+
+
+/**
+ * Route to get all the user info using a token
+ */
+export async function POST(request) {
+    const tokenData = request.cookies.get("token");
+
+    const userToken = tokenData ? tokenData.value : "";
+
+    let payload = {};
+
+    if (userToken.length > 0) {
+        // recover the payload (i.e. email and hashed password)
+        const { email, password } = recoverEmailPassword(userToken); // <- NOTE: password is hashed
+
+        // get all the user info/data from the database using the email and password
+        const { rows: users } = await sql`SELECT * FROM Users WHERE email = ${email} AND password = ${password}`;
+
+        
+
+        if (users.length === 0) {/* TODO: do something if there's no user with this email and password */}
+        else {
+            const userData = users[0];
+
+            // first_name, last_name, email, password, is_admin, is_manager) VALUES (${first_name}, ${last_name}, ${email}, ${hashedPassword}, ${is_admin}, ${is_manager}) RETURNING *
+            payload = {
+              first_name: userData.first_name,
+              last_name: userData.last_name,
+              email: userData.email,
+              is_admin: userData.is_admin,
+              is_manager: userData.is_manager
+            }
+        }
+    }
+
+    // GEt the user's email and password from the token #tokenDecryption
+
+
+    return Response.json({...payload, token: userToken});
 }
