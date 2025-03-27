fd53af738e014a24812061ceda02f20472adb4eb
catherine-tranchand <catherine.tranchand@laplateforme.io>
2025-03-27 16:42:05 +0100

    :test_tubes: Test me route via GET

--- app/auth/me/route.js
+++ app/auth/me/route.js
@@ -1,9 +1,8 @@
 import { sql } from "@vercel/postgres";
-import { recoverEmailPassword } from "@/utils/jwt"; 
-
-
+import { recoverEmailPassword } from "@/utils/jwt";
 
 // Handling the OPTIONS
+/*
 export async function OPTIONS(request) {
   return new Response(null, {
     status: 204,
@@ -18,10 +17,7 @@ export async function OPTIONS(request) {
   });
 }
 
-
-
-
-
+*/
 
 // route to find the user with an id
 /*
@@ -42,53 +38,58 @@ export async function POST(request){
 }
 */
 
-
-
 /**
  * Route to get all the user info using a token
  */
 export async function POST(request) {
-    
-    // Extracting token from Bearer <token> as userToken
-    const authHeader = request.headers.get("authorization");
-    const userToken = authHeader ? authHeader.split(" ")[1] : "";
+  // Extracting token from Bearer <token> as userToken
+  const authHeader = request.headers.get("authorization");
+  const userToken = authHeader ? authHeader.split(" ")[1] : "";
 
-    // TEMP: getting the token from the request's form data
-    // const formData = await request.formData();
-    // const userToken = formData.get("token");
+  // TEMP: getting the token from the request's form data
+  // const formData = await request.formData();
+  // const userToken = formData.get("token");
 
-    // const tokenData = request.cookies.get("token");
-    // const userToken = tokenData ? tokenData.value : "";
+  // const tokenData = request.cookies.get("token");
+  // const userToken = tokenData ? tokenData.value : "";
 
-    let payload = {};
+  let payload = {};
 
-    if (userToken.length > 0) {
-        // recover the payload (i.e. email and hashed password)
-        const { email, password } = recoverEmailPassword(userToken); // <- NOTE: password is hashed
+  if (userToken.length > 0) {
+    // recover the payload (i.e. email and hashed password)
+    const { email, password } = recoverEmailPassword(userToken); // <- NOTE: password is hashed
 
-        // get all the user info/data from the database using the email and password
-        const { rows: users } = await sql`SELECT * FROM Users WHERE email = ${email} AND password = ${password}`;
+    // get all the user info/data from the database using the email and password
+    const { rows: users } =
+      await sql`SELECT * FROM Users WHERE email = ${email} AND password = ${password}`;
 
-        
+    if (users.length === 0) {
+      /* TODO: do something if there's no user with this email and password */
+    } else {
+      const userData = users[0];
 
-        if (users.length === 0) {/* TODO: do something if there's no user with this email and password */}
-        else {
-            const userData = users[0];
-
-            // first_name, last_name, email, password, is_admin, is_manager) VALUES (${first_name}, ${last_name}, ${email}, ${hashedPassword}, ${is_admin}, ${is_manager}) RETURNING *
-            payload = {
-              user_id: userData.user_id, 
-              first_name: userData.first_name,
-              last_name: userData.last_name,
-              email: userData.email,
-              is_admin: userData.is_admin,
-              is_manager: userData.is_manager
-            }
-        }
+      // first_name, last_name, email, password, is_admin, is_manager) VALUES (${first_name}, ${last_name}, ${email}, ${hashedPassword}, ${is_admin}, ${is_manager}) RETURNING *
+      payload = {
+        user_id: userData.user_id,
+        first_name: userData.first_name,
+        last_name: userData.last_name,
+        email: userData.email,
+        is_admin: userData.is_admin,
+        is_manager: userData.is_manager,
+      };
     }
+  }
 
-    // GEt the user's email and password from the token #tokenDecryption
+  // GEt the user's email and password from the token #tokenDecryption
 
+  return Response.json({ ...payload, token: userToken });
+}
 
-    return Response.json({...payload, token: userToken});
+/**
+ * Route to get all the user info using a token
+ */
+export async function GET(request) {
+  // return Response.json({...payload, token: userToken});
+
+  return Response.json({ test: "hahahahah" });
 }
