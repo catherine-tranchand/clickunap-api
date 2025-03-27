5563630c51a10f4ae2858aa52f5484e2c50fe979
catherine-tranchand <catherine.tranchand@laplateforme.io>
2025-03-27 16:50:11 +0100

    :fire: update me route

--- app/auth/me/route.js
+++ app/auth/me/route.js
@@ -89,7 +89,38 @@ export async function POST(request) {
  * Route to get all the user info using a token
  */
 export async function GET(request) {
-  // return Response.json({...payload, token: userToken});
+  const { searchParams } = new URL(request.url);
+  const userToken = searchParams.get("token");
 
-  return Response.json({ test: "hahahahah" });
+  let payload = {};
+
+  if (userToken.length > 0) {
+    // recover the payload (i.e. email and hashed password)
+    const { email, password } = recoverEmailPassword(userToken); // <- NOTE: password is hashed
+
+    // get all the user info/data from the database using the email and password
+    const { rows: users } =
+      await sql`SELECT * FROM Users WHERE email = ${email} AND password = ${password}`;
+
+    if (users.length === 0) {
+      /* TODO: do something if there's no user with this email and password */
+    } else {
+      const userData = users[0];
+
+      // first_name, last_name, email, password, is_admin, is_manager) VALUES (${first_name}, ${last_name}, ${email}, ${hashedPassword}, ${is_admin}, ${is_manager}) RETURNING *
+      payload = {
+        user_id: userData.user_id,
+        first_name: userData.first_name,
+        last_name: userData.last_name,
+        email: userData.email,
+        is_admin: userData.is_admin,
+        is_manager: userData.is_manager,
+      };
+    }
+  }
+
+  // GEt the user's email and password from the token #tokenDecryption
+
+  return Response.json({ ...payload, token: userToken });
+
 }
